db92e9bd82f77e3fe45000754de1f4c2ae1a2065
Soumya Ranjan <soumyaranjan1812@gmail.com>
2025-10-01 03:21:00 +0530

    feat(rpc): add base jsonrpc types
    feat(rpc): implement base types

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,5 +1,7 @@
 mod coding;
 mod error;
+mod types;
 
 pub use coding::*;
 pub use error::*;
+pub use types::*;
---
+++ src/rpc/types.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+// Define type aliases for all the base types
+pub type Integer = i32;
+pub type UInteger = u32;
+pub type Decimal = u64;
+pub type LSPArray = Vec<LSPAny>;
+pub type LSPObject = HashMap<String, LSPAny>;
+
+/// This enum represents any usable value in the JSON rpc specification
+/// that is not null. This type is not in itself part of the spec,
+/// but allows for marking types that would never be nullable.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum LSPAny {
+    LSPObject(LSPObject),
+    LSPArray(LSPArray),
+    String(String),
+    Integer(Integer),
+    UInteger(UInteger),
+    Decimal(Decimal),
+    Boolean(bool),
+}
