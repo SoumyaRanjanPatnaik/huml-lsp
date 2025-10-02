1c749d66ebad9360734bdc899454bafbbb0fc62c
Soumya Ranjan <soumyaranjan1812@gmail.com>
2025-10-03 04:22:28 +0530

    refactor(logger): extract out logging functionlity

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 use huml_lsp::rpc::decode_from_buffer_rpc;
 use serde_json::Value;
 use std::{
+    env,
     error::Error,
     fs::File,
     io::{self, Write},
 };
 
+fn build_logger() -> impl FnMut(&str) -> () {
+    let log_file_path_result = env::var("HUML_LOG_PATH");
+    let log_path = log_file_path_result.as_deref().unwrap_or("/tmp/huml.log");
+    let mut log_file = File::create(log_path).unwrap();
+    let logger = move |message: &str| {
+        let _ = writeln!(log_file, "{message}");
+    };
+    logger
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut stdin_reader = io::stdin().lock();
     let mut read_buf = Vec::new();
+    let mut log = build_logger();
 
-    let mut log_file = File::create("/tmp/huml.log")?;
-    writeln!(log_file, "Started Server")?;
     loop {
-        writeln!(log_file, "waiting for request")?;
+        log("waiting for request");
         let value: Value = match decode_from_buffer_rpc(&mut stdin_reader, &mut read_buf) {
             Ok(val) => val,
             Err(e) => panic!("Invalid data format {e}"),
         };
-        writeln!(log_file, "Recieved request")?;
 
+        log("Recieved request");
         let json = serde_json::to_string_pretty(&value).expect("Serialization failed");
-        writeln!(log_file, "{json}")?;
+        log(&json);
     }
 }
